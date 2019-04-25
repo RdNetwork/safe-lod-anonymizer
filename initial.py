ecbad28cfed322dd01028af94ec9cbaa8240bda7
Rémy Delanaux <remy.delanaux@liris.cnrs.fr>
2019-04-25 14:48:26 +0200

    Fixing again...

--- initial.py
+++ initial.py
@@ -18,24 +18,24 @@ def main():
         f.write("Graph,IRIs,Blanks\n")
 
     for g in graph_list:
-        with open("./out/initial_deg_"+g+".csv","w+") as f:
-            f.write("Node,OutDegree,InDegree\n")
         print "Processing the " + g + " graph..."
         uri=PREFIX+g+"/"
 
-        print "\tFetching IRIs..."
-        start = time.time()
-        nb_iris = get_IRIs(sparql, uri)
-        end = time.time()
-        print "\tDone! (Took " + str(end-start) + " seconds)"
+        # print "\tFetching IRIs..."
+        # start = time.time()
+        # nb_iris = get_IRIs(sparql, uri)
+        # end = time.time()
+        # print "\tDone! (Took " + str(end-start) + " seconds)"
 
-        print "\tFetching blank nodes..."
-        start = time.time()
-        nb_blanks = get_IRIs(sparql, uri, True)
-        end = time.time()
-        print "\tDone! (Took " + str(end-start) + " seconds)"
+        # print "\tFetching blank nodes..."
+        # start = time.time()
+        # nb_blanks = get_IRIs(sparql, uri, True)
+        # end = time.time()
+        # print "\tDone! (Took " + str(end-start) + " seconds)"
 
         if not os.path.isfile("./out/initial_deg_"+g+".csv"):
+            with open("./out/initial_deg_"+g+".csv","w+") as f:
+                f.write("Node,OutDegree,InDegree\n")
             print "\tFetching degrees..."
             start = time.time()
             sparql.setQuery("DEFINE sql:log-enable 2 WITH <"+uri+"> SELECT ?n (COALESCE(MAX(?out),0) as ?outDegree) (COALESCE(MAX(?in),0) as ?inDegree) WHERE{ {SELECT ?n (COUNT(?p)  AS ?out) WHERE {?n ?p ?n2.} GROUP BY ?n} UNION {SELECT ?n (COUNT(?p)  AS ?in) WHERE {?n2 ?p ?n} GROUP BY ?n}}")
